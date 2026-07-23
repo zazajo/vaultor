b323261890977b00b779ac3f333efa327b5b0118
Joseph Edward <Josephedward201@gmail.com>
2026-07-23 23:59:28 +0100

    admin setup

--- backend/vaultor_v0/settings.py
+++ backend/vaultor_v0/settings.py
@@ -177,6 +177,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
     'staticfiles': {
         'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
     },
